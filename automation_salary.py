dcc1faea3ef53b5cbfc966cff8c97989f41c7699
Ali Ghrabi <alikghrabi@Alis-MacBook-Pro.local>
2025-02-21 21:21:24 +0200

    Taking inputs from user to automate it's salary

---
+++ automation_salary.py
@@ -0,0 +1,9 @@
+# Input Gathering
+
+salary = float(input("Enter your month salary: "))
+month_input = input("Enter the name of the month: ")
+percentages = {
+    "savings": float(input("Enter the percentage for savings: ")),
+    "rent": float(input("Enter the percentage for rent: ")),
+    "electricity": float(input("Enter the percentage for electricity: "))
+}
