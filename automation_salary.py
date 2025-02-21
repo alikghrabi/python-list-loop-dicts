71b4c6f0d7437e3c20d1fa8020532f260fc1303b
Ali Ghrabi <alikghrabi@Alis-MacBook-Pro.local>
2025-02-21 22:06:07 +0200

    defining a function to input a correct percentage

--- automation_salary.py
+++ automation_salary.py
@@ -2,8 +2,18 @@
 
 salary = float(input("Enter your month salary: "))
 month_input = input("Enter the name of the month: ")
+
+# function to check the percentages
+def get_percentage(name):
+    while True:
+        percentage = float(input(f"Enter the percentage for {name}: "))
+        if 0 <= percentage <= 100:
+            return percentage
+        else:
+            print("Percentage must be between 0 and 100. Please try again.")
+
 percentages = {
-    "savings": float(input("Enter the percentage for savings: ")),
-    "rent": float(input("Enter the percentage for rent: ")),
-    "electricity": float(input("Enter the percentage for electricity: "))
+    "savings": get_percentage("savings"),
+    "rent": get_percentage("rent"),
+    "electricity": get_percentage("electricity")
 }
